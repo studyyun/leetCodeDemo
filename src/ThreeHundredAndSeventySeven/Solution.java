f77a40d69fdb3d46efdd9ae231b31570c98e2a76
zhoushiyun <569824306@qq.com>
2021-06-16 11:00:16 +0800

    勤能补拙

--- src/ThreeHundredAndSeventySeven/Solution.java
+++ src/ThreeHundredAndSeventySeven/Solution.java
@@ -9,7 +9,20 @@ package ThreeHundredAndSeventySeven;
 public class Solution {
 
     public int combinationSum4(int[] nums, int target) {
-        return 0;
+        int[] dp = new int[target+1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (i >= num) {
+                    dp[i] = dp[i] + dp[i - num];
+                }
+            }
+        }
+        return dp[target];
+    }
+
+    public static void main(String[] args) {
+        
     }
     
 }
